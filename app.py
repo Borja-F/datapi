2a9257e78fe8b39b0b88eda9efab84dfa0817f6a
Borja Ferrer <jlarbfp@hotmail.com>
2023-09-25 19:20:26 +0200

    test new header

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def img_nsfw():
         if request.method == "POST":
             # Obtener el archivo de imagen desde el formulario
             img = request.files['image']
-            # user = request.headers["authorization"]
+            user = request.headers["authorization"]
             print(img)
             # Verificar si se seleccionó un archivo
             if img:
@@ -166,7 +166,7 @@ def img_nsfw():
                 # Meter todo en base de datos:
 
                 cols = {
-                    'user':"user",
+                    'user':user,
                     'img': data,
                     'response':str(response.json()['objects']),
                     'unsafe':str(response.json()['unsafe'])
