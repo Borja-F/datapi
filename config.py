acd8f73f12bbb8ce879585c6945d01067e314a1c
Borja Ferrer <jlarbfp@hotmail.com>
2023-09-28 12:25:03 +0200

    What the frick

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
 squri = os.getenv("squri")
+
 class Config:
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://fl0user:VASF0wG7HoPe@ep-winter-credit-54118564.eu-central-1.aws.neon.tech:5432/Apikey?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = squri
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://fl0user:VASF0wG7HoPe@ep-winter-credit-54118564.eu-central-1.aws.neon.tech:5432/Apikey?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_KEY_LENGTH = 32  # Set the desired length for API keys
